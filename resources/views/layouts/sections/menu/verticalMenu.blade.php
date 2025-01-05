556046be9a4fb184a0a073caafdafd1bfb5f3970
aldyardnsyh <aldyardiansyah@mail.ugm.ac.id>
2025-01-06 06:50:09 +0700

    feat: modified some functionality in vertival menu

--- resources/views/layouts/sections/menu/verticalMenu.blade.php
+++ resources/views/layouts/sections/menu/verticalMenu.blade.php
@@ -1,76 +1,61 @@
 <aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme" style="background-color: #343a40;">
-
   <!-- App Brand Section with White Background -->
   <div class="app-brand demo" style="background-color: #ffffff;">
-    <a href="{{url('/')}}" class="app-brand-link">
-      <span class="app-brand-logo demo">
-        @include('_partials.macros',["width"=>25,"withbg"=>'var(--bs-primary)'])
-      </span>
-    </a>
-
+    @include('_partials.macros', ["width" => 25, "withbg" => 'var(--bs-primary)'])
     <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-auto d-block d-xl-none">
       <i class="bx bx-chevron-left bx-sm align-middle"></i>
     </a>
   </div>
 
   <div class="menu-inner-shadow"></div>
-
   <ul class="menu-inner py-1">
     @foreach ($menuData[0]->menu as $menu)
-
     @if (isset($menu->menuHeader))
     <li class="menu-header small text-uppercase">
       <span class="menu-header-text">{{ __($menu->menuHeader) }}</span>
     </li>
-
     @else
-
     @php
     $activeClass = null;
     $currentRouteName = Route::currentRouteName();
 
     if ($currentRouteName === $menu->slug) {
-      $activeClass = 'active';
+    $activeClass = 'active';
+    } elseif (isset($menu->submenu)) {
+    $menuSlugs = is_array($menu->slug) ? $menu->slug : [$menu->slug];
+    foreach ($menuSlugs as $slug) {
+    if (str_starts_with($currentRouteName, $slug)) {
+    $activeClass = 'active open';
+    break;
     }
-    elseif (isset($menu->submenu)) {
-      if (gettype($menu->slug) === 'array') {
-        foreach($menu->slug as $slug){
-          if (str_contains($currentRouteName,$slug) and strpos($currentRouteName,$slug) === 0) {
-            $activeClass = 'active open';
-          }
-        }
-      }
-      else{
-        if (str_contains($currentRouteName,$menu->slug) and strpos($currentRouteName,$menu->slug) === 0) {
-          $activeClass = 'active open';
-        }
-      }
+    }
     }
     @endphp
 
-    <li class="menu-item {{$activeClass}}">
-      <a href="{{ isset($menu->url) ? url($menu->url) : 'javascript:void(0);' }}" class="{{ isset($menu->submenu) ? 'menu-link menu-toggle' : 'menu-link' }}" @if (isset($menu->target) and !empty($menu->target)) target="_blank" @endif>
+    <li class="menu-item {{ $activeClass }}">
+      <a href="{{ $menu->url ?? 'javascript:void(0);' }}"
+        class="{{ isset($menu->submenu) ? 'menu-link menu-toggle' : 'menu-link' }}"
+        @if (isset($menu->target) && !empty($menu->target)) target="_blank" @endif>
         @isset($menu->icon)
         <i class="{{ $menu->icon }}" style="color: #ffffff;"></i>
         @endisset
-        <div style="color: #ffffff;">{{ isset($menu->name) ? __($menu->name) : '' }}</div>
+        <div style="color: #ffffff;">{{ $menu->name ?? '' }}</div>
         @isset($menu->badge)
-          <div class="badge bg-{{ $menu->badge[0] }} rounded-pill ms-auto">{{ $menu->badge[1] }}</div>
+        <div class="badge bg-{{ $menu->badge[0] }} rounded-pill ms-auto">{{ $menu->badge[1] }}</div>
         @endisset
       </a>
 
       @isset($menu->submenu)
-      @include('layouts.sections.menu.submenu',['menu' => $menu->submenu])
+      @include('layouts.sections.menu.submenu', ['menu' => $menu->submenu])
       @endisset
     </li>
     @endif
     @endforeach
   </ul>
-
 </aside>
 
 <style>
-  /* Sidebar styling */
+  /* Sidebar Styling */
   #layout-menu {
     background-color: #343a40 !important;
   }
